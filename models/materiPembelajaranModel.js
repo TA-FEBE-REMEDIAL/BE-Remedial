b7a639f0a4d1551b0ffc207c86818f05c759c4f3
hasyim404 <muhaisim7@gmail.com>
2024-06-08 20:29:47 +0700

    add .gitignore

--- models/materiPembelajaranModel.js
+++ models/materiPembelajaranModel.js
@@ -3,25 +3,31 @@ const db = require("../config/database.js");
 
 const { DataTypes } = Sequelize;
 
-const Materi = db.define("materi", {
-  judul: {
-    type: DataTypes.STRING,
+const Materi = db.define(
+  "materi",
+  {
+    judul: {
+      type: DataTypes.STRING,
+    },
+    kategori: {
+      type: DataTypes.STRING,
+    },
+    deskripsi_materi: {
+      type: DataTypes.STRING,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+    },
+    kelas_rekomendasi: {
+      type: DataTypes.STRING,
+    },
+    kelas_id: {
+      type: DataTypes.INTEGER,
+    },
   },
-  kategori: {
-    type: DataTypes.STRING,
-  },
-  deskripsi_materi: {
-    type: DataTypes.STRING,
-  },
-  image_url: {
-    type: DataTypes.STRING,
-  },
-  kelas_rekomendasi: {
-    type: DataTypes.STRING,
-  },
-  kelas_id: {
-    type: DataTypes.INTEGER,
-  },
-});
+  {
+    freezeTableName: true,
+  }
+);
 
 module.exports = Materi;
